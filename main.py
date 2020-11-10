a201ffe0496b22ca1d4b2fee48ca1565cac660c4
ehdus <ehdus1201@naver.com>
2020-11-10 15:50:17 +0900

    2주차2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-file = open('COVID 19.txt', 'w')
+def average(list):
+    return(sum(list)/len(list))
 
-f = open('COVID 19.txt', 'r')
-line = f.readline()ㅋ
+list = [77,63,75,64,73,75,114,69,54,72,58,98,12,84,110,47,73]
+print('평균값:',average(list))
